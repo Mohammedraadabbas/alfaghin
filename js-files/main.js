4aa1b8347a63eb21e0cd36846f99662d7ce2ff14
Mohammedraadabbas <104260916+Mohammedraadabbas@users.noreply.github.com>
2022-06-24 06:52:10 -0700

    Delete js-files directory

--- js-files/main.js
+++
@@ -1,6 +0,0 @@
-let hamburger = document.querySelector(".hamburger-menu");
-let navBar = document.querySelector(".nav-bar");
-
-hamburger.addEventListener("click", () => {
-    navBar.classList.toggle("open");
-})
